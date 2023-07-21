3edd7c1169c558ef221250ea66123c19404b933b
Ashish Singh <ashishsingh.1899@gmail.com>
2023-07-21 12:44:15 +0530

    created user Model

---
+++ models/user.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: [true, "Please provide your name"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Please provide your name"],
+  },
+  email: {
+    type: String,
+
+    required: [true, "Please provide your email"],
+    unique: true,
+  },
+  username: {
+    type: String,
+    required: [true, "Please provide your username"],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide your password"],
+  },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
